a9798eec00f1c60ad5ff6f688c8a7f04abf9159c
hosein asadi <work.hoseinasadi@gmail.com>
2020-11-07 10:19:55 +0330

    Add files via upload
    add sample project

---
+++ TcpServerKitSample/TcpServerKitSample/MyRoom.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TcpServerKit.Core;
+
+namespace TcpServerKitSample
+{
+    class MyRoom : Room
+    {
+        public MyRoom(int roundIndex) : base(roundIndex)
+        {
+
+        }
+
+
+        public override void GameCompleted()
+        {
+            Console.WriteLine("GameCompleted");
+        }
+
+        public override void NewUserJoined(User user)
+        {
+            Console.WriteLine("NewUserJoined");
+        }
+
+        public override void RoomReadyForStart(List<User> users)
+        {
+            if (AllUsersJoined)
+                Console.WriteLine("RoomReadyForStart : AllUsersJoined");
+            else
+                Console.WriteLine("RoomReadyForStart : !AllUsersJoined");
+        }
+
+        public override void RoundStarted(ushort roundId)
+        {
+            Console.WriteLine("RoundStarted");
+        }
+
+        public override void UserExited(User user)
+        {
+            Console.WriteLine("UserExited");
+        }
+
+        public override void UserKicked(User user)
+        {
+            Console.WriteLine("UserKicked");
+        }
+    }
+}
---
+++ TcpServerKitSample/TcpServerKitSample/MyUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using TcpServerKit.Core;
+
+namespace TcpServerKitSample
+{
+    class MyUser : User
+    {
+        public MyUser()
+        {
+        }
+
+        public MyUser(TcpClient tcpClient) : base(tcpClient)
+        {
+        }
+    }
+}
